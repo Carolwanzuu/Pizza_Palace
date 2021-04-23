166afef1827c5c5d6b5e14665644f54b99987871
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-23 15:39:49 +0300

    removed a part of the toppings form

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-$(document).ready(function(event){
+// $(document).ready(function(event){
     
-})
+// })
+
+// function Pizza(size, crust, toppings){
+//     this.size = ['small', 'medium','large'];
+//     this.crust = ['crispy','stuffed','gluten-free'];
+//     this.toppings = ['Pepperoni','mushrooms','gorgonzola','capers','broccoli'];
+// }
 // function Size(small, medium, large){
 //     this.small = small;
 //     this.medium = medium;
@@ -11,8 +17,28 @@ $(document).ready(function(event){
 //     this.stuffed = stuffed;
 //     this.gluten_free = gluten_free;
 // };
+// function Toppings(pepperoni, mushrooms, gorgonzola, capers, broccoli){
+//     this.pepperoni = pepperoni;
+//     this.mushrooms = mushrooms;
+//     this.gorgonzola = gorgonzola;
+//     this.capers = capers;
+//     this.broccoli = broccoli;
+// };
+
 function Pizza(size, crust, toppings){
-    this.size = ['small', 'medium','large'];
-    this.crust = ['crispy','stuffed','gluten-free'];
-    this.toppings = ['Pepperoni','mushrooms','gorgonzola','capers','broccoli'];
-}
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+    }
+
+    $(document).ready(function(){
+        $("#submit").click(function(event){
+            event.preventDefault();
+            let size =$("option#size").val();
+            let crust = $("input#crust").val();
+            let toppings = $("input#toppings").val();
+            console.log(size, crust, toppings);
+        })
+    
+    })
+//  let Order1 = new Pizza(size, crust,toppings);  
