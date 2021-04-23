cbfb563fa59465388a1134d28370253fd3555463
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-23 07:37:05 +0300

    included a function constructor for pizza

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(event){
     
+})
+// function Size(small, medium, large){
+//     this.small = small;
+//     this.medium = medium;
+//     this.large = large;
+// };
+// function Crust(crispy, stuffed, gluten_free){
+//     this.crispy = crispy;
+//     this.stuffed = stuffed;
+//     this.gluten_free = gluten_free;
+// };
+function Pizza(size, crust, toppings){
+    this.size = ['small', 'medium','large'];
+    this.crust = ['crispy','stuffed','gluten-free'];
+    this.toppings = ['Pepperoni','mushrooms','gorgonzola','capers','broccoli'];
 }
-function Size(small, medium, large){
-    this.small = small;
-    this.medium = medium;
-    this.large = large;
-};
-function
